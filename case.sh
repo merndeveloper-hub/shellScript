6edf891aed92a9a6344343c687d633cd7e42dfbd
Owais Yameen <wangchaoiq@gmail.com>
2024-05-04 16:32:29 +0500

    shellscript for devops

--- case.sh
+++ case.sh
@@ -4,16 +4,16 @@ action={1}
 
 case ${action} in
     start)
-    echo "going to start"
+    echo "going to start project"
     ;;
     stop)
-    echo "going to stop"
+    echo "going to stop project" 
     ;;
     reload)
-    echo "going to reload"
+    echo "going to reload project"
     ;;
     restart)
-    echo "going to restart"
+    echo "going to restart project"
     ;;
     *)
      echo "plaese enter correct command"
